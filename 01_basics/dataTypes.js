47c82d2a5fd57376d90ab08c7ffa8a9f3de7ff29
Faiz Ul Haq <117098201+faizulhaq7@users.noreply.github.com>
2025-02-25 20:35:58 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Invalid Password")   // We are working with node, not in browser
+
+let name = "Abhigyan"
+let age = 21
+let isLoggedIn = false
+
+// number, bigint
+// string => ""
+// boolean => yes/no
+// null => standalone value, it is an object
+// undefined => value not assigned yet, it is a type
+// symbol => unique, used in React
+
+// object
+
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(name));
+
+console.log(typeof(null));
+console.log(typeof(undefined));
