9708bad98a283c307debe5a71d8b90512d5d4516
Faiz Ul Haq <117098201+faizulhaq7@users.noreply.github.com>
2025-02-26 16:51:58 +0000

    strings updated

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -13,6 +13,10 @@ const adminName = new String("faiz ul haq");    // Here, adminName is an object
 console.log(adminName.length)   // returns length of the String object
 console.log(adminName.substring(0, 4))     // returns a part of the String within the mentioned index
 // Prints from 0th [f] to 3rd [z] index, 4th is excluded
+// console.log(adminName.charAt(0));   // f - returns char at the specifies index
+let cuteName = "    A s i m   ";
+console.log(cuteName.trim());   // removes the extra spaces/indent from start and end
+
 
 let value = "Gian"
 // console.log(typeof value);  // string
