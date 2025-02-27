38cd404e2931aa4d1f42af93707b5a0678826748
Faiz Ul Haq <117098201+faizulhaq7@users.noreply.github.com>
2025-02-27 20:51:47 +0000

    date and time

---
+++ 01_basics/date_time.js
@@ -0,0 +1,8 @@
+const myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toDateString())   // THu Feb 27 2025
+// console.log(myDate.toLocaleDateString())    // 2/27/2025
+// console.log(myDate.toString())
+
+let myCreatedDate = new Date(2004, 2, 27)
+console.log(myCreatedDate);
