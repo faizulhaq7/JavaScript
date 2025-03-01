156ebbbe5152e1a960a0230fe9fe8accbdfa9ab7
Faiz Ul Haq <117098201+faizulhaq7@users.noreply.github.com>
2025-03-01 10:59:20 +0000

    dates and time

--- 01_basics/date_time.js
+++ 01_basics/date_time.js
@@ -1,8 +1,33 @@
 const myDate = new Date()
+// console.log(typeof myDate);     // Object (FAQ in interviews)
+
 // console.log(myDate)
 // console.log(myDate.toDateString())   // THu Feb 27 2025
 // console.log(myDate.toLocaleDateString())    // 2/27/2025
 // console.log(myDate.toString())
+// console.log(myDate.toISOString());   // ISO and JSON give the same output
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
 
-let myCreatedDate = new Date(2004, 2, 27)
-console.log(myCreatedDate);
+let myCreatedDate = new Date(2004, 2, 27)   // 2=> March (Month starts from 0 in JS)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());      // Sat Mar 27 2004
+// console.log(myCreatedDate.getTime());
+
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate.toLocaleDateString())
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+// console.log(`Day is ${newDate.getDay()} and month is ${newDate.getMonth()+1}`)
+
+console.log(newDate.toLocaleString('default',{
+    // weekday: "short",   // Sat
+    weekday: "long"     // Saturday
+}));
