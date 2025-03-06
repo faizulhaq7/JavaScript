5cc3d17c1dedf3e4025003313a7ba07b8db7d3fc
Faiz Ul Haq <117098201+faizulhaq7@users.noreply.github.com>
2025-03-06 18:47:10 +0000

    while & dowhile loop

---
+++ 04_iterators/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+// let i = 1
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// const arr = ["Faiz", "Naveen", "Shivanshu"]
+// let i = 0
+// while (i < arr.length) {
+//     console.log("My friend is", arr[i]);
+//     i++;
+// }
+
+//  do-while loop (rarely used)
+// let score = 15;
+// do {
+//     console.log("Score is", score);
+//     score++;
+// } while (score < 10){
+//     console.log("Final score is", score);
+// }
