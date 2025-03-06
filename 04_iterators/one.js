08c7722917ce0ae3d5d20637f7dca678b73cefcc
Faiz Ul Haq <117098201+faizulhaq7@users.noreply.github.com>
2025-03-06 17:46:31 +0000

    for loop

---
+++ 04_iterators/one.js
@@ -0,0 +1,28 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// Iterating over an Array
+// const array = ["Faiz", "Tauheed", "Abhigyan", "Shreyansh"]
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element);
+// }
+
+// break statement
+for (let i = 0; i < 10; i++) {
+    if (i == 5) {
+        console.log("5 is not acceptable. Moving out!");
+        break;
+    }
+    console.log(i);
+}
+
+// continue statement
+// for (let i = 0; i < 10; i++) {
+//     if (i == 5) {
+//         console.log("5 is not acceptable. Ignoring once");
+//         continue;
+//     }
+//     console.log(i);
+// }
