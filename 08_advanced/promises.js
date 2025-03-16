38e1e03671cb75e0112d41344b538b3b27cadc5b
Faiz Ul Haq <117098201+faizulhaq7@users.noreply.github.com>
2025-03-16 17:13:56 +0000

    promises done

--- 08_advanced/promises.js
+++ 08_advanced/promises.js
@@ -70,3 +70,63 @@ promiseFour.then(function(user){
 // .finally(function(){
 //     console.log('The promise is either resolved or rejected.');
 // })
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false;
+        // let error = true;       // The promise is either resolved or rejected.
+        if (!error) {
+            resolve({username: "Tauheed", email: "tauheed@gmail.com"});
+        } else {
+            reject("Error: Something went wrong");
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive();
+
+// async function getAllUsers() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// };
+
+// getAllUsers();
+
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then(function(response){
+    return response.json;
+})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log(error);
+});
+
+
+// promise.all
+
+const firstPromise = Promise.resolve(77);
+const secondPromise = 18;
+const thirdPromise = new Promise(function(resolve, reject){
+    setTimeout(resolve, 100, "Faiz")
+});
+
+Promise.all([firstPromise, secondPromise, thirdPromise]).then((values) => {
+    console.log(values);
+})
